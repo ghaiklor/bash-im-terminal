4e01338aa436c5271471801b596dd554739d9c15
Eugene Obrezkov <ghaiklor@gmail.com>
2017-04-18 16:45:12 +0300

    feat(package): Implement decoding of HTML entities

--- src/common/getQuotes.js
+++ src/common/getQuotes.js
@@ -1,4 +1,5 @@
 const cheerio = require('cheerio');
+const he = require('he');
 const Quote = require('../Quote');
 
 /**
@@ -8,14 +9,14 @@ const Quote = require('../Quote');
  * @returns {Array<Quote>}
  */
 module.exports = function (html) {
-  const $ = cheerio.load(html, {decodeEntities: false});
+  const $ = cheerio.load(html);
   const quotes = $('.quote');
 
   return quotes
     .filter((i, elem) => $(elem).find('.text').html())
     .map((i, elem) => {
       const id = $(elem).find('.id').text().replace('#', '');
-      const text = $(elem).find('.text').html().split('<br>').join('\n');
+      const text = he.decode($(elem).find('.text').html()).split('<br>').join('\n');
       const rating = $(elem).find('.rating').text();
       const date = $(elem).find('.date').text();
 
